8b7d952948603e984808af12c107aa38205a9015
Shan Rizvi <shanrizvi2@gmail.com>
2019-01-08 16:33:13 -0600

    first commit

---
+++ application.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body>
+
+
+
+<?php
+    header("Location: /Users/shanrizvi/Documents/ScavHunt/index.html");
+?>
+
+</body>
+</html>
