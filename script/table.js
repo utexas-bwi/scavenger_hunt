596c8440da4345838a617f8c71e9ef9e62164180
Mayuri Raja <mraja7@utexas.edu>
2019-03-05 17:46:50 -0600

    added clickable, constructable table to make hunt

--- script/table.js
+++ script/table.js
@@ -1,3 +1,6 @@
+
+/* functions to add to task table */
+
 function addRow(cell) {
     const table = cell.parentNode.parentNode.parentNode;
     const row = document.getElementById("tr-hide").cloneNode(true);
@@ -14,4 +17,15 @@ function removeRow(cell) {
 function saveChanges() {
     const table = document.getElementById("task-table");
     // TODO: export data to SQL TaskType database 
+}
+
+/* functions to add to hunt table */
+
+function addTask(task) {
+    const table = document.getElementById("hunt-table");
+    const row = document.getElementById("tr-hide").cloneNode(true);
+    row.childNodes[1].textContent = task;
+    row.classList.remove("hide");
+    row.classList.remove("table-line");
+    table.appendChild(row);
 }
