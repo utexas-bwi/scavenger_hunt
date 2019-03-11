31878fc4c4587e097cfa14f10b826e627f092fac
Mayuri Raja <mraja7@utexas.edu>
2019-03-11 12:16:37 -0500

    add editable UI to task table page

--- script/table.js
+++ script/table.js
@@ -1,18 +1,37 @@
 
-/* function to make cell editable */
+/* 
+ * toggle editabiity of cell: makes editable if not editable and vice versa
+ *
+ * parameters:
+ *      span: the span containing the pencil glyphicon that was clicked
+ *          expects the nesting to be
+ *          <tr>
+ *              <td>
+ *                  <span> pencil icon </span>
+ *              </td>
+ *          </tr>
+ *      start: the cell to start at when making editable
+ *          1 = first child, 3 = second, 5 = third, etc.
+ *          start at 1 if you want whole row to be editable
+ *
+ * preconditions:
+ *      - span follows proper nesting (noted above)
+ *      - start is a number > 1 and < number of child nodes
+ *      - tr is formatted to have two uneditable child nodes at end
+ *          (in our case, the pencil and x icons)
+ * */
 
-function editable(span) {
+function editable(span, start) {
     const tr = span.parentNode.parentNode;
-    console.log(tr);
     if (span.classList.contains("glyphicon-pencil")) {
-        for (let i = 3; i < tr.childNodes.length - 4; i = i + 2) {
+        for (let i = start; i < tr.childNodes.length - 4; i = i + 2) {
             tr.childNodes[i].setAttribute("contentEditable","true");
             tr.childNodes[i].classList.add("editable");
         }
         span.classList.remove("glyphicon-pencil");
         span.classList.add("glyphicon-ok");
     } else {
-        for (let i = 3; i < tr.childNodes.length - 4; i = i + 2) {
+        for (let i = start; i < tr.childNodes.length - 4; i = i + 2) {
             tr.childNodes[i].setAttribute("contentEditable","false");
             tr.childNodes[i].classList.remove("editable");
         }
@@ -29,6 +48,7 @@ function addRow(cell) {
     const row = document.getElementById("tr-hide").cloneNode(true);
     row.classList.remove("hide");
     row.classList.remove("table-line");
+    editable(row.childNodes[11].childNodes[1], 1);
     table.appendChild(row);
 }
 
@@ -50,6 +70,6 @@ function addTask(task) {
     row.childNodes[1].textContent = task;
     row.classList.remove("hide");
     row.classList.remove("table-line");
-    editable(row.childNodes[5].childNodes[1]);
+    editable(row.childNodes[5].childNodes[1], 3);
     table.appendChild(row);
 }
