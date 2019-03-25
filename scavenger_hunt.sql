ee49931a88d3518562f2bab63e9038f18d586be8
Mayuri Raja <mraja7@utexas.edu>
2019-03-25 13:11:11 -0500

    updated database backup

--- scavenger_hunt.sql
+++ scavenger_hunt.sql
@@ -16,6 +16,90 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `hunt_instructions_table`
+--
+
+DROP TABLE IF EXISTS `hunt_instructions_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hunt_instructions_table` (
+  `hunt_id` int(11) DEFAULT NULL,
+  `hunt_instr_id` int(11) NOT NULL AUTO_INCREMENT,
+  `task_type` varchar(20) DEFAULT NULL,
+  `param_value` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`hunt_instr_id`),
+  KEY `hunt_id` (`hunt_id`),
+  KEY `task_type` (`task_type`),
+  CONSTRAINT `hunt_instructions_table_ibfk_1` FOREIGN KEY (`hunt_id`) REFERENCES `hunt_table` (`hunt_id`),
+  CONSTRAINT `hunt_instructions_table_ibfk_2` FOREIGN KEY (`task_type`) REFERENCES `task_table` (`task_type`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hunt_instructions_table`
+--
+
+LOCK TABLES `hunt_instructions_table` WRITE;
+/*!40000 ALTER TABLE `hunt_instructions_table` DISABLE KEYS */;
+INSERT INTO `hunt_instructions_table` VALUES (1,1,'Color Shirt','green');
+/*!40000 ALTER TABLE `hunt_instructions_table` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hunt_table`
+--
+
+DROP TABLE IF EXISTS `hunt_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hunt_table` (
+  `hunt_id` int(11) NOT NULL AUTO_INCREMENT,
+  `hunt_name` varchar(255) DEFAULT NULL,
+  `release_date` date DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`hunt_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `hunt_table_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_table` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hunt_table`
+--
+
+LOCK TABLES `hunt_table` WRITE;
+/*!40000 ALTER TABLE `hunt_table` DISABLE KEYS */;
+INSERT INTO `hunt_table` VALUES (1,'my hunt','2019-01-01',1);
+/*!40000 ALTER TABLE `hunt_table` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `task_param_table`
+--
+
+DROP TABLE IF EXISTS `task_param_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `task_param_table` (
+  `param_id` int(11) NOT NULL AUTO_INCREMENT,
+  `param_name` varchar(255) DEFAULT NULL,
+  `possible_values` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`param_id`),
+  UNIQUE KEY `param_name` (`param_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `task_param_table`
+--
+
+LOCK TABLES `task_param_table` WRITE;
+/*!40000 ALTER TABLE `task_param_table` DISABLE KEYS */;
+INSERT INTO `task_param_table` VALUES (1,'object','can, pencil, shoe'),(2,'color','yellow, red, blue, green'),(3,'','');
+/*!40000 ALTER TABLE `task_param_table` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `task_table`
 --
 
@@ -25,12 +109,15 @@ DROP TABLE IF EXISTS `task_table`;
 CREATE TABLE `task_table` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `task_type` varchar(20) DEFAULT NULL,
-  `param_name` set('color','object') DEFAULT NULL,
+  `param_name` varchar(255) DEFAULT NULL,
   `proof_type` varchar(200) DEFAULT NULL,
   `score` int(11) DEFAULT NULL,
   `description` varchar(500) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `task_type` (`task_type`),
+  KEY `param_name` (`param_name`),
+  CONSTRAINT `task_table_ibfk_1` FOREIGN KEY (`param_name`) REFERENCES `task_param_table` (`param_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -58,7 +145,7 @@ CREATE TABLE `task_table_backup` (
   `score` int(11) DEFAULT NULL,
   `description` varchar(500) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -70,6 +157,31 @@ LOCK TABLES `task_table_backup` WRITE;
 INSERT INTO `task_table_backup` VALUES (1,'Color Shirt','color','picture (jpg) of the person wearing the shirt',100,'Take a picture of a person who is wearing a specific color shirt.'),(2,'Target Search','object','picture (jpg) of the specified object',100,'Take a picture of the specified object.'),(3,'Human Following','','picture (jpg) of the human and picture (jpg) of the trajectories walked by the human and robot',100,'Follow a human for a certain distance.'),(4,'Object Delivery','','time-stamped description of object by person who gives the task and time-stamped description of object by person at ending location',150,'Find a person who is willing to specify an object to deliver from one location to another. Objects and locations can be selected through the robot UI in real time. Once the robot delivers the object, it should find a person at the ending location who can describe the object.');
 /*!40000 ALTER TABLE `task_table_backup` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `user_table`
+--
+
+DROP TABLE IF EXISTS `user_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_table` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_table`
+--
+
+LOCK TABLES `user_table` WRITE;
+/*!40000 ALTER TABLE `user_table` DISABLE KEYS */;
+INSERT INTO `user_table` VALUES (1,'mayuri','pwd');
+/*!40000 ALTER TABLE `user_table` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -80,4 +192,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-03-13 17:46:11
+-- Dump completed on 2019-03-25 13:10:57
