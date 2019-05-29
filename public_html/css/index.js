c814350d1edafc577490b45a27469b2fee8b41ed
Stefan deBruyn <stefandebruyn@utexas.edu>
2019-05-29 13:11:40 -0500

    Fix nav bar navigation from index. Nav bar now populates correctly on every page

--- public_html/css/index.js
+++ public_html/css/index.js
@@ -1,30 +1,55 @@
 firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
+  var path = window.location.pathname;
+  var page = path.split("/").pop();
 
+  // User is signed in
+  if (user) {
     // document.getElementById("user_div").style.display = "block";
     // document.getElementById("login_div").style.display = "none";
 
     // document.getElementById('navbar').innerHTML = 'components/navBar.html';
-    $("#navbar").load("components/navBar.html");
+    $("#navbar").load(htmlComponentsPath + "navBarAuth.html");
 
     console.log("im in");
 
+    window.onload = function() {
+      var e = document.getElementById("navbar-rules");
+      e.setAttribute('href', 'public_html/' + e.getAttribute('href'));
+      e = document.getElementById("navbar-task");
+      e.setAttribute('href', 'public_html/' + e.getAttribute('href'));
+      e = document.getElementById("navbar-leaderboard");
+      e.setAttribute('href', 'public_html/' + e.getAttribute('href'));
+      e = document.getElementById("navbar-contact");
+      e.setAttribute('href', 'public_html/' + e.getAttribute('href'));
+      e = document.getElementById("navbar-userhunts");
+      e.setAttribute('href', 'public_html/' + e.getAttribute('href'));
+    };
+  // No user is signed in
   } else {
-    // No user is signed in.
-
     // document.getElementById("user_div").style.display = "none";
     // document.getElementById("login_div").style.display = "block";
 
-    $("#navbar").load("components/navBar_noauth.html");
+    $("#navbar").load(htmlComponentsPath + "navBarNoAuth.html");
 
     console.log("no user :(");
 
+    // Points each of the navbar items into the public_html folder
+    window.onload = function() {
+      var e = document.getElementById("navbar-rules");
+      e.setAttribute('href', 'public_html/' + e.getAttribute('href'));
+      e = document.getElementById("navbar-leaderboard");
+      e.setAttribute('href', 'public_html/' + e.getAttribute('href'));
+      e = document.getElementById("navbar-contact");
+      e.setAttribute('href', 'public_html/' + e.getAttribute('href'));
+      e = document.getElementById("navbar-register");
+      e.setAttribute('href', 'public_html/' + e.getAttribute('href'));
+      e = document.getElementById("navbar-login");
+      e.setAttribute('href', 'public_html/' + e.getAttribute('href'));
+    };
   }
 });
 
 function login(){
-
   var userEmail = document.getElementById("email_field").value;
   var userPass = document.getElementById("password_field").value;
 
