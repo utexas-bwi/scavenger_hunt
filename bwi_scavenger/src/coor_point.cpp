38c72028332996788ce226b1799037aa462a7462
jsuriadinata <jsuriadinata@utexas.edu>
2019-06-06 14:21:08 -0500

    added test file to generate coordinate points of the robot

---
+++ bwi_scavenger/src/coor_point.cpp
@@ -0,0 +1,14 @@
+#include <ros/ros.h>
+#include <geometry_msgs>
+
+void explore(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &curPose){
+  sdt::cout << "Coordinate Point: (" << curPose->pose->pose->position->x << ", " << curPose->pose->pose->position->x << ")" std::endl;;
+}
+
+int main(int arc, char **argv){
+
+  ros::init(argc, argv, "coor_point");
+
+  ros::Subscriber locationSub = node.subscribe("/amcl_pose", 1, explore);
+
+}
