7a34ca57a8d376c78e76538e7e5a52aa34a5f75d
jsuriadinata <jsuriadinata@utexas.edu>
2019-06-14 13:00:18 -0500

    update closest location for task upon starting the task each time

--- bwi_scavenger/src/move_node.cpp
+++ bwi_scavenger/src/move_node.cpp
@@ -6,6 +6,8 @@
 #include <std_msgs/String.h>
 
 int closest = -1;
+tf::TransformListener *listener;
+std::string gridFrameId;
 
 void stop(const bwi_scavenger::RobotStop::ConstPtr &data){
     ROS_INFO("[move_node] Cancel goal");
@@ -28,28 +30,34 @@ void move(const bwi_scavenger::RobotMove::ConstPtr &data){
   }
 }
 
-void getMapId(const nav_msgs::OccupancyGrid::ConstPtr &grid){
-  tf::TransformListener *tfl = new tf::TransformListener();
-  rm = new RobotMotion(grid->header.frame_id, *tfl);
+void start(const std_msgs::String::ConstPtr &msg){
+  if (msg -> data == "Find Object") {
+    ROS_INFO("Setting closest location");
+    tf::StampedTransform robotTransform;
+    listener->waitForTransform(gridFrameId, "base_link", ros::Time::now(), ros::Duration(4));
+    listener->lookupTransform(gridFrameId, "base_link", ros::Time(0), robotTransform);
+    float x = robotTransform.getOrigin().x();
+    float y = robotTransform.getOrigin().y();
+    ROS_INFO("[move_node] position of robot: (%f, %f)", x, y);
 
-  tf::StampedTransform robotTransform;
-  tfl->waitForTransform(grid->header.frame_id, "base_link", ros::Time::now(), ros::Duration(4));
-  tfl->lookupTransform(grid->header.frame_id, "base_link", ros::Time(0), robotTransform);
-  float x = robotTransform.getOrigin().x();
-  float y = robotTransform.getOrigin().y();
-  ROS_INFO("[move_node] position of robot: (%f, %f)", x, y);
-
-  float minDistance = std::numeric_limits<float>::max();
-  for(int i = 0; i < 7; i++){
-    std::pair<float, float> coordinates = environment_location_coordinates[static_cast<environment_location>(i)];
-    float distance = sqrt(pow(coordinates.first - x, 2) + pow(coordinates.second - y, 2));
-    ROS_INFO("[move_node] distance to location %d: %f", i, distance);
-    if (distance < minDistance){
-      minDistance = distance;
-      closest = i;
+    float minDistance = std::numeric_limits<float>::max();
+    for(int i = 0; i < 7; i++){
+      std::pair<float, float> coordinates = environment_location_coordinates[static_cast<environment_location>(i)];
+      float distance = sqrt(pow(coordinates.first - x, 2) + pow(coordinates.second - y, 2));
+      ROS_INFO("[move_node] distance to location %d: %f", i, distance);
+      if (distance < minDistance){
+        minDistance = distance;
+        closest = i;
+      }
     }
   }
+}
 
+void getMapId(const nav_msgs::OccupancyGrid::ConstPtr &grid){
+  ROS_INFO("Creating RobotMotion");
+  listener = new tf::TransformListener();
+  gridFrameId = grid->header.frame_id;
+  rm = new RobotMotion(gridFrameId, *listener);
 }
 
 int main(int argc, char **argv){
@@ -60,6 +68,7 @@ int main(int argc, char **argv){
 
   ros::Subscriber findObjectSub = moveNode.subscribe(TPC_MOVE_NODE_GO, 1, move);
   ros::Subscriber stopMoveSub = moveNode.subscribe(TPC_MOVE_NODE_STOP, 1, stop);
+  ros::Subscriber startFindObject = moveNode.subscribe(TPC_MAIN_NODE_TASK_START, 1, start);
 
   movePub = moveNode.advertise<std_msgs::Bool>(TPC_MOVE_NODE_FINISHED, 1);
 
