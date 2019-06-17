271ebe46664dbcd973de16754b83e368bb99e0f7
Stefan deBruyn <stefandebruyn@utexas.edu>
2019-06-17 17:36:57 -0500

    Fix trailing files

--- website/public_html/css/index.js
+++ website/public_html/css/index.js
@@ -1,35 +1,57 @@
-// Page updates when the authentication state changes
-firebase.auth().onAuthStateChanged(function(user) {
-  /*setTimeout(function() {
-    const message = document.getElementById("alert-msg");
-    message.classList.remove("hidden-msg");
-    message.classList.remove("alert-danger");
-    message.classList.add("alert-success");
-    message.textContent = "A verification email has been sent to haha yes.";
-    setTimeout(function() {
-        message.classList.add("hidden-msg");
-        message.classList.remove("alert-success");
-    }, 4000);
-  }, 500);*/
-
+// Point nav bar items into public_html folder if on index
+function indexNav(page) {
   var path = window.location.pathname;
   var page = path.split("/").pop();
-  var verified = !!user && firebase.auth().currentUser.emailVerified;
-  var authRestrictedPages = [
+
+  if (page == "index.html" || page == "") {
+    var e = document.getElementById("navbar-rules");
+    if (e) e.setAttribute('href', 'public_html/' + e.getAttribute('href'));
+    e = document.getElementById("navbar-task");
+    if (e) e.setAttribute('href', 'public_html/' + e.getAttribute('href'));
+    e = document.getElementById("navbar-leaderboard");
+    if (e) e.setAttribute('href', 'public_html/' + e.getAttribute('href'));
+    e = document.getElementById("navbar-contact");
+    if (e) e.setAttribute('href', 'public_html/' + e.getAttribute('href'));
+    e = document.getElementById("navbar-userhunts");
+    if (e) e.setAttribute('href', 'public_html/' + e.getAttribute('href'));
+    e = document.getElementById("navbar-register");
+    if (e) e.setAttribute('href', 'public_html/' + e.getAttribute('href'));
+    e = document.getElementById("navbar-login");
+    if (e) e.setAttribute('href', 'public_html/' + e.getAttribute('href'));
+  }
+}
+
+// Page updates when the authentication state changes
+firebase.auth().onAuthStateChanged(function(user) {
+  var path = window.location.pathname;
+  var page = path.split("/").pop();
+  var verified = user && user.emailVerified;
+  var verifyRestrictedPages = [
     "userhunts.html",
     "task.html"
   ];
+  var logoutRestrictedPages = [
+    "login.html"
+  ];
+
+  // Force redirect if user is logged out and on a restricted page
+  if (!verified && verifyRestrictedPages.includes(page))
+    window.location = "../index.html";
+
+  // Force redirect if user is logged in and on a restricted page
+  if (user && logoutRestrictedPages.includes(page))
+    window.location = "../index.html";
 
   // Nav bar changes based on user verification
   if (!user)
-    $("#navbar").load(htmlComponentsPath + "navBarNoAuth.html");
+    $("#navbar").load(htmlComponentsPath + "navBarNoAuth.html", indexNav);
   else if(!verified)
-    $("#navbar").load(htmlComponentsPath + "navBarUnverified.html");
+    $("#navbar").load(htmlComponentsPath + "navBarUnverified.html", indexNav);
   else
-    $("#navbar").load(htmlComponentsPath + "navBarAuth.html");
+    $("#navbar").load(htmlComponentsPath + "navBarAuth.html", indexNav);
 
-  // Send verification email if necessary
-  if (!!user && !verified && page == "register.html") {
+  if (user && !verified && page == "register.html") {
+    // Send verification email if necessary
     user.sendEmailVerification().then(function() {
       const message = document.getElementById("alert-msg");
       message.classList.remove("hidden-msg");
@@ -39,80 +61,27 @@ firebase.auth().onAuthStateChanged(function(user) {
       setTimeout(function() {
           message.classList.add("hidden-msg");
           message.classList.remove("alert-success");
-      }, 4000);
+          window.location = "../index.html";
+      }, 2000);
     }).catch(function(error) {
       console.log("Failed to send verification email!");
     });
-  }
 
-  // Bounce the user if they don't qualify to be on this page
-  if (authRestrictedPages.includes(page) && (!user || !verified))
-    window.location = "../index.html";
-
-  // User just signed in
-  if (user) {
     // Attempt to add the user to the SQL database if they haven't been already
-    if (page == "register.html") {
-      $.ajax({
-        type: "POST",
-        url: "../../script/register.php",
-        data: {
-          "user_id": user.uid.hashCode(),
-          "email": user.email,
-          "university": window.university,
-          "pass_hash": document.getElementById("pass").value.hashCode()
-        },
-        success: function(data) {
-          if (page == "register.html" && verified)
-            window.location = "userhunts.html";
-        },
-        failure: function(data) {
-          console.log("Something went wrong with new user reg");
-        }
-      });
-    }
-
-    // Point each of the nav bar items into the public_html folder (necessary
-    // for navigation from index page to work)
-    window.onload = function() {
-      var e = document.getElementById("navbar-rules");
-      e.setAttribute('href', 'public_html/' + e.getAttribute('href'));
-      e = document.getElementById("navbar-leaderboard");
-      e.setAttribute('href', 'public_html/' + e.getAttribute('href'));
-      e = document.getElementById("navbar-contact");
-      e.setAttribute('href', 'public_html/' + e.getAttribute('href'));
-      if(verified){
-        e = document.getElementById("navbar-task");
-        e.setAttribute('href', 'public_html/' + e.getAttribute('href'));
-        e = document.getElementById("navbar-userhunts");
-        e.setAttribute('href', 'public_html/' + e.getAttribute('href'));
+    $.ajax({
+      type: "POST",
+      url: "../../script/register.php",
+      data: {
+        "user_id": user.uid.hashCode(),
+        "email": user.email,
+        "university": window.university,
+        "pass_hash": document.getElementById("pass").value.hashCode()
       }
-    };
-
-    // Graceful redirect to userhunts page upon login
-    if (page == "login.html")
-      window.location = "userhunts.html";
-
-    console.log("im in");
-  // User just signed out
-  } else {
-    $("#navbar").load(htmlComponentsPath + "navBarNoAuth.html");
-
-    window.onload = function() {
-      var e = document.getElementById("navbar-rules");
-      e.setAttribute('href', 'public_html/' + e.getAttribute('href'));
-      e = document.getElementById("navbar-leaderboard");
-      e.setAttribute('href', 'public_html/' + e.getAttribute('href'));
-      e = document.getElementById("navbar-contact");
-      e.setAttribute('href', 'public_html/' + e.getAttribute('href'));
-      e = document.getElementById("navbar-register");
-      e.setAttribute('href', 'public_html/' + e.getAttribute('href'));
-      e = document.getElementById("navbar-login");
-      e.setAttribute('href', 'public_html/' + e.getAttribute('href'));
-    };
-
-    console.log("no user :(");
+    });
   }
+
+  if (verified && page == "login.html")
+    window.location = "userhunts.html";
 });
 
 // Called when clicking login on login page
