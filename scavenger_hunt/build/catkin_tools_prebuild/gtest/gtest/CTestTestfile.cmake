4a0b95c154ebb747ed44aa6ca3b0fe4787adc275
Stefan deBruyn <stefandebruyn@utexas.edu>
2019-06-18 10:14:51 -0500

    Cleaning up catkin bloat

--- scavenger_hunt/build/catkin_tools_prebuild/gtest/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/bwilab/scavenger_hunt/scavenger_hunt/build/catkin_tools_prebuild/gtest/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- scavenger_hunt/src/client_test.cpp
+++ scavenger_hunt/src/client_test.cpp
@@ -13,11 +13,11 @@ using namespace rapidxml;
 
 int main(int argc, char** argv) {
   ScavengerHuntClient client("stefandebruyn@utexas.edu", "sick robots");
-  ScavengerHunt *hunt = client.get_hunt("Longhorn Hunt");
+  ScavengerHunt *hunt = client.get_hunt("BWI Lab Hunt");
 
   if (hunt != nullptr) {
     for (int i = 0; i < hunt->size(); i++)
-      std::cout << hunt->get_task(i); // (*hunt)[i] also works
+      std::cout << *hunt->get_task(i); // (*hunt)[i] also works
 
     // client.send_proof("bottle.png", (*hunt)[0]);
   }
