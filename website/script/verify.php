3bb8c0922a3eb34e5e46acea9a661cf22e17fdb2
Stefan deBruyn <stefandebruyn@utexas.edu>
2019-06-18 11:20:46 -0500

    Cleaner validation page when no proofs

--- website/script/verify.php
+++ website/script/verify.php
@@ -1,13 +1,15 @@
 <?php
   include 'connect.php';
   $dbh = connect();
-  
+
   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   // obtains the files that have not yet been verified by a user
   $query = "SELECT * FROM proof_table WHERE verified = 0";
   $stmt = $dbh->query($query);
   $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+  $proofs = 0;
 
   // goes through the filenames of proofs that have not been verified and displays on page
   while($item = $stmt -> fetch()){
@@ -16,5 +18,9 @@
     $huntInstrId = $item['hunt_instr_id'];
     $uploaderId = $item['uploader_id'];
     include 'components/image-verify.html';
+    $proofs += 1;
   }
+
+  if ($proofs == 0)
+    include 'components/no-proofs.html';
 ?>
