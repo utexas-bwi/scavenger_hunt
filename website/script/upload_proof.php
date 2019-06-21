ab0e5daaa706af94c1dd608509e8830a24ea335c
Stefan deBruyn <stefandebruyn@utexas.edu>
2019-06-21 12:40:10 -0500

    Proof upload now rejects proofs for expired hunts

--- website/script/upload_proof.php
+++ website/script/upload_proof.php
@@ -10,6 +10,37 @@ $hash = hash("sha256", $_FILES["image"]["tmp_name"] . $_FILES["image"]["name"] .
 $filetype = strtolower(pathinfo($_FILES["image"]["name"], PATHINFO_EXTENSION));
 $target_filename = date('y-m-d') . date("H:i:s") . "-" . $hash . "." . $filetype;
 
+// Check if the hunt in question is ongoing
+$ongoing = true;
+
+try {
+  $stmt = $dbh->query("select * from hunt_instructions_table where hunt_instr_id=" . $_POST["instr_id"]);
+  $stmt->setFetchMode(PDO::FETCH_ASSOC);
+  $reference_instr = $stmt->fetch();
+
+  if ($reference_instr) {
+    $stmt = $dbh->query("select * from hunt_table where hunt_id=" . $reference_instr["hunt_id"]);
+    $reference_hunt = $stmt->fetch();
+
+    if ($reference_hunt) {
+      $release_date = $reference_hunt["release_date"];
+      $end_date = $reference_hunt["end_date"];
+      $current_date = date('y-m-d');
+      if ($current_date >= $release_date && $current_date <= $end_date)
+        $ongoing = true;
+    }
+  }
+
+} catch (PDOException $e) {
+  echo "[send_proof] An error occurred: " . $e->getMessage() . "\n";
+  die();
+}
+
+if (!$ongoing) {
+  echo("[send_proof] The specified hunt has expired!");
+  die();
+}
+
 // Check file size
 if ($_FILES["image"]["size"] > 500000) {
   echo "[send_proof] Upload failed: file exceeds size limit.\n";
@@ -36,12 +67,12 @@ try {
     move_uploaded_file($_FILES["image"]["tmp_name"], $proof_dir . $target_filename);
 
     // Log new proof in database
-    echo $_POST["time"];
     $query = "insert into proof_table values (0, '" . $user['user_id'] . "', '" . $target_filename . "', " . $_POST["instr_id"] . ", " . $_POST["time"] . ", 0, 0)";
     $stmt = $dbh->query($query);
   } else
-    echo "[send_proof] Specified user not found in database!";
+    echo "[send_proof] Specified user not found in database!\n";
 } catch (PDOException $e) {
   echo "[send_proof] An error occurred: " . $e->getMessage() . "\n";
+  die();
 }
 ?>
