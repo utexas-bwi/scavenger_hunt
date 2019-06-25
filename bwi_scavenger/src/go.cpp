ea92ef0655a613a284eea32c18fa0721e44eb356
Stefan deBruyn <stefandebruyn@utexas.edu>
2019-06-25 10:44:24 -0500

    Add recall script

--- bwi_scavenger/src/go.cpp
+++ bwi_scavenger/src/go.cpp
@@ -3,10 +3,23 @@
 #include <bwi_scavenger/RobotStop.h>
 #include <bwi_scavenger/robot_motion.h>
 #include <ros/ros.h>
+#include <string.h>
 
 static ros::Publisher pub_move;
 
 int main(int argc, char **argv) {
+  if (argc < 2) {
+    ROS_ERROR("Usage: go <location>");
+    exit(0);
+  }
+
+  environment_location l;
+
+  if (strcmp(argv[1], "fellows"))
+    l = FELLOW_COMPUTERS;
+  else if (strcmp(argv[1], "lab"))
+    l = BWI_LAB_RIGHT;
+
   ros::init(argc, argv, "go");
   ros::NodeHandle nh;
 
@@ -16,9 +29,8 @@ int main(int argc, char **argv) {
 
   bwi_scavenger::RobotMove msg;
   msg.type = 0;
-  msg.location = FELLOW_COMPUTERS;
+  msg.location = l;
   pub_move.publish(msg);
 
-  ros::spin();
-  return 0;
+  ros::spinOnce();
 }
