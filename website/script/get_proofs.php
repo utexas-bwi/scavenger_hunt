7ee25f65e0074e9e428ab2c6e13660b7eb5dda85
Stefan deBruyn <stefandebruyn@utexas.edu>
2019-06-25 12:20:00 -0500

    Proof feedback only returns validated proofs

--- website/script/get_proofs.php
+++ website/script/get_proofs.php
@@ -7,16 +7,16 @@ $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 try {
     global $dom, $node, $parnode;
     // get the current task from the database
-    $hunt = $_POST['hunt_name'];
-    $task = $_POST['task_name'];
-    $param = $_POST['param'];
-    $user_email = $_POST['user_email'];
-    // $user_email = jsuriadinata@utexas.edu;
-    // $hunt = "Dijkstra Hunt 1";
-    // $task = "Find Object";
-    // $param = "chair";
+    // $hunt = $_POST['hunt_name'];
+    // $task = $_POST['task_name'];
+    // $param = $_POST['param'];
+    // $user_email = $_POST['user_email'];
+    $user_email = "stefandebruyn@utexas.edu";
+    $hunt = "BWI Lab Hunt";
+    $task = "Find Object";
+    $param = "chair";
 
-    $stmt = $dbh -> query("SELECT * FROM user_table WHERE user_email = '$user_email'");
+    $stmt = $dbh -> query("SELECT * FROM user_table WHERE email = '$user_email'");
     $stmt -> setFetchMode(PDO::FETCH_ASSOC);
     $user_id = ($stmt -> fetch())['user_id'];
 
@@ -35,10 +35,10 @@ try {
 
     while($taskList = $stmt -> fetch()){
         $hunt_instr_id = $taskList['hunt_instr_id'];
-        $query = "SELECT * FROM proof_table WHERE hunt_instr_id = $hunt_instr_id AND uploader_id = $user_id";
+        $query = "SELECT * FROM proof_table WHERE hunt_instr_id = $hunt_instr_id AND uploader_id = $user_id" . " AND verified=1";
         $stmt = $dbh->query($query);
         $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        
+
         $getTask = $dbh -> query("SELECT * FROM hunt_instructions_table WHERE hunt_instr_id = $hunt_instr_id");
         $getTask -> setFetchMode(PDO::FETCH_ASSOC);
         $task = $getTask -> fetch();
@@ -48,14 +48,19 @@ try {
         $getFormat = $dbh -> query("SELECT * FROM task_table WHERE task_type = '$taskType'");
         $getFormat -> setFetchMode(PDO::FETCH_ASSOC);
         $proof_format = ($getFormat -> fetch())['proof_type'];
-    
-        $xml->startElement('task');
-        $xml->writeAttribute('type', $taskType);
-        $xml->writeAttribute('parameter', $paramValue);
-        $xml->writeAttribute('proof_format', $proof_format);
-        $xml->writeAttribute('hunt_instr_id', $hunt_instr_id);
-    
+
+        $head_node = false;
+
         while ($proofTable = $stmt->fetch()) {
+            if (!$head_node) {
+              $head_node = true;
+              $xml->startElement('task');
+              $xml->writeAttribute('type', $taskType);
+              $xml->writeAttribute('parameter', $paramValue);
+              $xml->writeAttribute('proof_format', $proof_format);
+              $xml->writeAttribute('hunt_instr_id', $hunt_instr_id);
+            }
+
             $correct = $proofTable['correct'];
             $filename = $proofTable['filename'];
             $time = $proofTable['time_to_complete'];
@@ -65,7 +70,9 @@ try {
             $xml->writeAttribute('filename', "../proof/".$filename);
             $xml->endElement();
         }
-        $xml->endElement();
+
+        if ($head_node)
+          $xml->endElement();
     }
 
 	$xml->endDocument();
