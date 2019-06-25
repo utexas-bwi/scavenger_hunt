578e982aa965ae6d4d39ab6a3bed7b518e1212cf
Stefan deBruyn <stefandebruyn@utexas.edu>
2019-06-25 15:42:11 -0500

    Fix misleading example code in API docs

--- scavenger_hunt/src/client_example.cpp
+++ scavenger_hunt/src/client_example.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char** argv) {
   // Create a new client object and sign in with your website credentials.
-  ScavengerHuntClient client("your@email.com", "password");
+  ScavengerHuntClient client("stefandebruyn@utexas.edu", "sick robots");
 
   // Create an empty vector of tasks for the client to populate with hunt data.
   std::vector<Task> tasks;
@@ -12,7 +12,7 @@ int main(int argc, char** argv) {
   // Scavenger hunts are downloaded by name. We've created a never-expiring hunt
   // called Bottle Hunt to test with. It contains a single task to find a bottle
   // and take a picture of it.
-  client.get_hunt("Bottle Hunt", tasks);
+  client.get_hunt("BWI Lab Hunt", tasks);
 
   // Task objects contain all of the information necessary to complete them.
   for (int i = 0; i < tasks.size(); i++) {
@@ -43,9 +43,8 @@ int main(int argc, char** argv) {
     bool is_correct = proof.get_correct(); // Was I right?
 
     // The original file uploaded as proof can be retrieved if necessary
-    client.download_proof_material(proof, "my_validated_proofs/"); // Folder to download into
-    std::ofstream proof_file;
-    proof_file.open("my_validated_proofs/" + proof.get_filename());
-    proof_file.close();
+    std::string download_directory = "/home/my_downloaded_proofs";
+    client.download_proof_material(proof, download_directory);
+    std::string path_to_proof = download_directory + "/" + proof.get_filename();
   }
 }
