5bd91fe2d88239a422ce1da045295fd619e9a282
jsuriadinata <jsuriadinata@utexas.edu>
2019-07-01 15:52:12 -0500

    original clusterer improved

--- bwi_scavenger/src/dbscan.cpp
+++ bwi_scavenger/src/dbscan.cpp
@@ -1,12 +1,6 @@
 #include <bwi_scavenger/dbscan.h>
-#include <iostream>
-#include <math.h>
 
-#define INCORRECT_THRESHOLD 50
-#define CORRECT_THRESHOLD 75
-
-typedef std::vector<point> Neighbors;
-
+typedef std::vector<point*> Neighbors;
 
 /**
   Creates a new Clusterer object that stores the database of poses, radius eps and
@@ -23,44 +17,37 @@ Clusterer::Clusterer(float** db, int num_dimensions, int size_of_database){
     for(int j = 0; j < num_dimensions; j++){
       temp.coordinate[j] = db[i][j];
     }
-    // temp.verification = db[i].verification;
     temp.label = UNDEFINED;
-    // temp.num = i;
-    // robot_positions.push_back(db[i].robot_pose.position);
     database[i] = temp;
   }
 }
 
 Clusterer::~Clusterer(){
-  // delete &database;
+  // delete database;
   // delete &cluster_list;
 }
 
 /**
   Calculates the distance between the two 3D points
 */
-static float calculate_distance(float* p1, float* p2, int num_dimensions){
+float calculate_distance(float* p1, float* p2, int num_dimensions){
   int total = 0;
-  for(int i = 0; i < num_dimensions; i++)
+  for(int i = 0; i < num_dimensions; i++){
     total += pow((p1[i] - p2[i]), 2);
-<<<<<<< HEAD
-  return abs(pow(total, 1.0 / p1.size()));
-=======
-  // std::cout << "distance" <<  std::to_string(abs(pow(total, 1.0 / num_dimensions))) << std::endl;
-  return abs(pow(total, 1.0 / num_dimensions));
->>>>>>> e23f41da04eae9f14c5893b1add17288a8e7ff69
+  }
+  return sqrt(total);
 }
 
 /**
   Gets all the points that are within radius from the point
 */
-Neighbors get_neighbors(point* db, float* p, float eps, int size_of_database, int num_dimensions){
+Neighbors get_neighbors(point* db, point &p, float eps, int size_of_database, int num_dimensions){
   Neighbors n;
   for(int i = 0; i < size_of_database; i++){
-    point p2 = db[i];
-    float dist = calculate_distance(p, p2.coordinate, num_dimensions);
-    if(dist < eps && dist != 0)
-      n.push_back(p2);
+    point &p2 = db[i];
+    float dist = calculate_distance(p.coordinate, p2.coordinate, num_dimensions);
+    if(dist < eps && p2.num != p.num)
+      n.push_back(&p2);
   }
   return n;
 }
@@ -68,50 +55,37 @@ Neighbors get_neighbors(point* db, float* p, float eps, int size_of_database, in
 /**
   Expands the cluster for each of the original point's neighbors
 */
-void expand_cluster(point* db, cluster current_cluster, Neighbors n, float eps, int minPoints, int size_of_database, int num_dimensions){
-
+void expand_cluster(point* db, Cluster &current_cluster, Neighbors n, float eps, int minPoints, int size_of_database, int num_dimensions){
+  std::cout << " expand_cluster " << std::endl;
   for(int j = 0; j < n.size(); j++){
-    point secondary_point = n[j];
+    point &secondary_point = *(n[j]);
 
     // if already labelled "noise" it cannot be a main cluster point, thus is set to edge point
     if(secondary_point.label == NOISE){
-      current_cluster.list.push_back(secondary_point);
+      // std::cout << "noise" << std::endl;
       secondary_point.label = IN_CLUSTER;
+      current_cluster.add_to_list(secondary_point);
     }
-
+    
     // continues if already in a cluster or was previously defined as noise and now is in this cluster (edge point)
     if(secondary_point.label != UNDEFINED)
       continue;
-<<<<<<< HEAD
 
-    Neighbors n2 = get_neighbors(db, secondary_point.coordinate, eps);
-=======
-    
-    Neighbors n2 = get_neighbors(db, secondary_point.coordinate, eps, size_of_database, num_dimensions);
->>>>>>> e23f41da04eae9f14c5893b1add17288a8e7ff69
+    Neighbors n2 = get_neighbors(db, secondary_point, eps, size_of_database, num_dimensions);
     secondary_point.label = IN_CLUSTER;
+    current_cluster.add_to_list(secondary_point);
     // main cluster gets points that are also with the min points required
     if(n2.size() < minPoints)
       continue;
-<<<<<<< HEAD
-
-    // std::cout << "expanding cluster again" << std::endl;
-    // // labels each neighbors as in a cluster and add to cluster's list
-    // for(int k = 0; k < n2.size(); k++){
-    //   n2[k].label = IN_CLUSTER;
-    //   current_cluster.list.push_back(n2[k]);
-    //   //continue to expand the cluster with this point
-    //   expand_cluster(db, current_cluster, get_neighbors(db, n2[k].coordinate, eps), eps, minPoints);
-
-    // }
-=======
     
     // labels each neighbors as in a cluster and add to cluster's list
-    for(int k = 0; k < n2.size(); k++){
-      n2[k].label = IN_CLUSTER;
-      current_cluster.list.push_back(n2[k]);
+    for(int k = 0; k < n2.size(); k++){   
+      if(secondary_point.label != UNDEFINED)
+        continue;
+      point &point2 = *(n2[k]);
+      point2.label = IN_CLUSTER;
+      current_cluster.add_to_list(secondary_point);
     }
->>>>>>> e23f41da04eae9f14c5893b1add17288a8e7ff69
   }
 }
 
@@ -122,26 +96,27 @@ void expand_cluster(point* db, cluster current_cluster, Neighbors n, float eps, 
   @param eps radius to check for neighbors at and main cluster radius
   @param minPoints minimum number of points required to create a cluster
 */
-int Clusterer::get_clusters(float eps, int minPoints){
+int Clusterer::generate_clusters(float eps, int minPoints){
 
   this->eps = eps;
   this->minPoints = minPoints;
-
+  
   int count = 0; // cluster id
+
+  for(int i = 0; i < size_of_database; i++){
+    point &current_point = database[i];
+    current_point.num = i;
+  }
   // goes through the entire dataset to create clusters
   for(int i = 0; i < size_of_database; i++){
-    point current_point = database[i];
+    point &current_point = database[i];
 
     // only continue if label has not been placed yet
-    if(current_point.label != UNDEFINED)
+    if(current_point.label != UNDEFINED){
       continue;
-<<<<<<< HEAD
-
-    Neighbors n = get_neighbors(database, current_point.coordinate, eps);
-=======
-    
-    Neighbors n = get_neighbors(database, current_point.coordinate, eps, size_of_database, num_dimensions);
->>>>>>> e23f41da04eae9f14c5893b1add17288a8e7ff69
+    }
+      
+    Neighbors n = get_neighbors(database, current_point, eps, size_of_database, num_dimensions);
     // point is "noise" if it does not have enough neighbors;
     if(n.size() < minPoints){
       current_point.label = NOISE;
@@ -149,131 +124,77 @@ int Clusterer::get_clusters(float eps, int minPoints){
     }
 
     current_point.label = IN_CLUSTER;
+    Cluster cluster(count, current_point);
     count++;
-    cluster cluster;
-    // std::cout << "Count " << std::to_string(count) << std::endl;
-    cluster.num = count;
-    // original cluster is the point and its neighbors
-    cluster.list = n;
-    cluster_list.push_back(cluster);
     // expand cluster with the current point's neighbors
+    std::cout << std::to_string(current_point.num);
     expand_cluster(database, cluster, n, eps, minPoints, size_of_database, num_dimensions);
+    cluster_list.push_back(cluster);
   }
   return count;
 }
 
-<<<<<<< HEAD
 /**
-  Obtains all of the clusters above a certain threshold of "incorrectness"
-  Returns a vector of cluster numbers that are considered "incorrect"
+  Returns a the cluster associated with the cluster number
 */
-std::vector<int> Clusterer::get_incorrect(){
-  std::vector<int> result;
+Cluster Clusterer::get_cluster(int cluster_num){
+  return cluster_list[cluster_num];
+}
+/**
+  Returns the cluster number of the largest cluster in the data set
+*/
+Cluster Clusterer::get_largest_cluster(){
+  int max = 0;
+  Cluster *maxCluster;
   for(int i = 0; i < cluster_list.size(); i++){
-    cluster current_cluster = cluster_list[i];
-    if(current_cluster.num_incorrect / current_cluster.list.size() > INCORRECT_THRESHOLD)
-      result.push_back(current_cluster.num);
+    int size = cluster_list[i].size();
+    if(size > max){
+      max = size;
+      maxCluster = &cluster_list[i];
+    }
   }
-  return result;
+  return *maxCluster;
 }
 
-/**
-  Obtains all of the clusters above a certain threshold of "correctness"
-  Returns a vector of cluster numbers that are considered "correct"
-*/
-
-std::vector<int> Clusterer::get_correct(){
-  std::vector<int> result;
-  for(int i = 0; i < cluster_list.size(); i++){
-    cluster current_cluster = cluster_list[i];
-    if(current_cluster.num_correct / current_cluster.list.size() > CORRECT_THRESHOLD)
-      result.push_back(current_cluster.num);
-  }
-  return result;
+Cluster::Cluster(int num, point p){
+  this->num = num;
+  std::vector<point> newList;
+  list = newList;
+  list.push_back(p);
 }
 
-// /**
-//   Gets the mean location based on the robot positions from the database
-// */
-// geometry_msgs::Point mean_robot_location(cluster cluster, std::vector<geometry_msgs::Point> robot_positions){
-//   double totalX = 0;
-//   double totalY = 0;
-//   int length = cluster.list.size();
+Cluster::~Cluster(){}
 
-//   for(int i = 0; i < length; i++){
-//     int num = cluster.list[i].num;
-//     geometry_msgs::Point pos = robot_positions[i];
-//     totalX += pos.x;
-//     totalY += pos.y;
-//   }
-//   geometry_msgs::Point result;
-//   result.x = totalX / length;
-//   result.y = totalY / length;
-//   return result;
-// }
-
-// /**
-//   Calculates the closest location to the current position of the robot that
-//   contains a "correct" point. Returns a pose of the correct position
-
-//   @param robot_pose pose of robot
-// */
-// geometry_msgs::Point Clusterer::closest_correct(geometry_msgs::Pose robot_pose){
-
-//   double min_distance = DBL_MAX;
-//   int cluster_num = -1;
-//   geometry_msgs::Point min_distance_point;
-
-//   for(int i = 0; i < cluster_list.size(); i++){
-//     geometry_msgs::Point robot_position = mean_robot_location(cluster_list[i], robot_positions);
-//     double distance = calculate_distance(robot_position, robot_pose.position);
-//     if(distance < min_distance){
-//       min_distance = distance;
-//       cluster_num = i;
-//       min_distance_point = robot_position;
-//     }
-//   }
-
-//   return min_distance_point;
-// }
-
-/**
-=======
 /** 
->>>>>>> e23f41da04eae9f14c5893b1add17288a8e7ff69
   Returns whether or not a point is contained in a specific cluster
 
   @param point Point that is being checked
-  @param cluster_num cluster number that the point is being checked against
+  @param cluster cluster  that the point is being checked against
 */
-bool Clusterer::in_cluster(float* point, int cluster_num){
-  cluster cluster = get_cluster(cluster_num);
-  for(int i = 0; i < cluster.list.size(); i++){
-    float distance = calculate_distance(cluster.list[i].coordinate, point, num_dimensions);
+bool Clusterer::in_cluster(float* point, Cluster cluster){
+  // std::cout << "coor: " << std::to_string(point[0]) << ", " << std::to_string(point[1]) << std::endl;
+  for(int i = 0; i < cluster.size(); i++){
+    float distance = calculate_distance(cluster.get_point(i).coordinate, point, num_dimensions);
+    // std::cout << "hi hi" << std::endl;
     if(distance < eps)
       return true;
   }
   return false;
 }
 
-/**
-  Returns a the cluster associated with the cluster number
-*/
-cluster Clusterer::get_cluster(int cluster_num){
-  return cluster_list[cluster_num];
+void Cluster::add_to_list(point p){
+  list.push_back(p);
 }
-/**
-  Returns the cluster number of the largest cluster in the data set
-*/
-int Clusterer::get_largest_cluster(){
-  int max = 0;
-  int maxCluster;
-  for(int i = 0; i < cluster_list.size(); i++){
-    int size = cluster_list[i].list.size();
-    if(size > max){
-      max = size;
-      maxCluster = cluster_list[i].num;
-    }
-  }
-  return maxCluster;
+
+int Cluster::cluster_num(){
+  return num;
 }
+
+int Cluster::size(){
+  return list.size();
+}
+
+point Cluster::get_point(int pos){
+  return list[pos];
+}
+
