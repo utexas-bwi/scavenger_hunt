65d19b3f5c372f80c98dceb09e0165062ea229f8
jsuriadinata <jsuriadinata@utexas.edu>
2019-07-15 15:35:17 -0500

    Fix memory leak for object cluster and object clusterer

--- scavenger_hunt/src/bag_to_mp4_test.cpp
+++ scavenger_hunt/src/bag_to_mp4_test.cpp
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include <scavenger_hunt/scavenger_hunt.h>
 
 int main(int argc, char** argv){
   std::string nameOfBag = "three_cups.bag";
@@ -6,4 +7,14 @@ int main(int argc, char** argv){
   std::string command = "cd /home/bwilab/scavenger_hunt_ws/; rm " + nameOfVideo + "; ";
   command += "./rosbag2video.py -o " + nameOfVideo + " " + nameOfBag;
   system(command.c_str());  
+
+  std::cin.ignore();
+  
+  ScavengerHuntClient client("jsuriadinata@utexas.edu", "Tr3asure");
+
+  std::vector<Task> tasks;
+
+  client.get_hunt("Video Hunt", tasks);
+
+  proof_id_t id = client.send_proof("/home/bwilab/scavenger_hunt_ws/video.mp4", tasks[0], 60.0);
 }
