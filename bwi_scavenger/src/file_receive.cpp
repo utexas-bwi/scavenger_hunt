045f799ad836392d79dc7fafa886131ed4e9cdfb
jsuriadinata <jsuriadinata@utexas.edu>
2019-07-23 09:54:38 -0500

    Send all files over

--- bwi_scavenger/src/file_receive.cpp
+++ bwi_scavenger/src/file_receive.cpp
@@ -1,20 +1,4 @@
-#include <ros/ros.h>
-
-#include <bwi_scavenger/globals.h>
-
-#include <bwi_scavenger_msgs/DatabaseFile.h>
-
-#include <iostream>
-#include <fstream>
-
-static const std::string WS_PATH = "/home/jsuriadinata/scavenger_hunt";
-static const std::string DNROS_PATH = WS_PATH + "/src/darknet_ros/darknet_ros";
-
-static const std::string TAG_DNROS_WEIGHTS = "dnros_weights";
-static const std::string TAG_DNROS_CFG = "dnros_cfg";
-static const std::string TAG_DNROS_MODEL_YAML = "dnros_model_yaml";
-static const std::string TAG_DNROS_ROS_YAML = "dnros_ros_yaml";
-static const std::string TAG_DNROS_LAUNCH = "dnros_launch";
+#include <bwi_scavenger/transfer_files.h>
 
 void receive_cb(const bwi_scavenger_msgs::DatabaseFile msg){
   std::string destination;
--- bwi_scavenger/src/file_send.cpp
+++ bwi_scavenger/src/file_send.cpp
@@ -1,22 +1,49 @@
-#include <ros/ros.h>
+#include <bwi_scavenger/transfer_files.h>
 
-#include <bwi_scavenger/globals.h>
-#include <bwi_scavenger/file_editor.h>
+ros::Publisher pub_send_proofs_file;
 
-#include <bwi_scavenger_msgs/DatabaseFile.h>
+void send_file(int tag){
+  std::string source;
+  std::string name;
+  std::string type = "Find_Object";
 
-#include <iostream>  
-#include <fstream>  
+  bwi_scavenger_msgs::DatabaseFile msg;
 
-int main(int argc, char** argv){
-  ros::init(argc, argv, "file_send");
-  ros::NodeHandle nh;
+  if (tag == 0){
+    msg.tag = TAG_DNROS_WEIGHTS;
+    source = DNROS_PATH + "/yolo_network_config/weights";
+    name = type + ".weights";
+  }
+  else if (tag == 1){
+    msg.tag = TAG_DNROS_CFG;
+    source = DNROS_PATH + "/yolo_network_config/cfg";
+    name = type + ".cfg";
+  }
+  else if (tag == 2){
+    msg.tag = TAG_DNROS_MODEL_YAML;
+    source = DNROS_PATH + "/config";
+    name = type + ".yaml";
+  }
+  else if (tag == 3){
+    msg.tag = TAG_DNROS_ROS_YAML;
+    source = DNROS_PATH + "/config";
+    name = type + "-ros.yaml";
+  }
+  else if (tag == 4){
+    msg.tag = TAG_DNROS_LAUNCH;
+    source = DNROS_PATH + "/launch";
+    name = "darknet_ros_" + type + ".launch";
+  }
+  else {
+    ROS_ERROR("Can't send file: undetermined tag");
+    return;
+  }
 
-  ros::Publisher pub_send = nh.advertise<bwi_scavenger_msgs::DatabaseFile>(TPC_TRANSFER_NODE_SEND_FILE, 1);
-  ros::Duration(2.0).sleep();
+  msg.name = name;
 
-  std::string name = PROOF_DATABASE_PATH;
-  std::ifstream fl(name);  
+  // copy file into byte array
+  std::string filepath = source + name;
+  std::ifstream fl(filepath);  
   fl.seekg(0, std::ios::end);  
   size_t len = fl.tellg();  
   char *ret = new char[len];  
@@ -24,12 +51,21 @@ int main(int argc, char** argv){
   fl.read(ret, len);  
   fl.close();  
 
-  bwi_scavenger_msgs::DatabaseFile msg;
-  msg.name = PROOF_DATABASE_PATH;
-
   for(int i = 0; i < len; i++)
     msg.data.push_back(ret[i]);
-  pub_send.publish(msg);
 
+  pub_send_proofs_file.publish(msg);
+}
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "file_send");
+  ros::NodeHandle nh;
+
+  pub_send_proofs_file = nh.advertise<bwi_scavenger_msgs::DatabaseFile>(TPC_TRANSFER_NODE_SEND_FILE, 1);
+  ros::Duration(2.0).sleep();
+
+  for(int i = 0; i < NUM_FILES; i++)
+    send_file(i);
+  
   ros::spin();
 }
