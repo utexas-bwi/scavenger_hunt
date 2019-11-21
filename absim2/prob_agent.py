c1590318fec48b5ebce535b554dcd0f510023cde
Eric Tang <eltang@cs.utexas.edu>
2019-11-21 00:13:37 -0600

    Improve probability-based agent
    - Calculate score based on probability of finding at least one object
    - Don't commit to a best path

--- absim2/prob_agent.py
+++ absim2/prob_agent.py
@@ -4,34 +4,36 @@ import world
 
 class ProbAgent(agent.Agent):
     """The probability-greedy agent visits the location with the highest
-    aggregate probabiliy of finding new objects. If two locations are equally
+    probability of finding any object. If two locations are equally
     favorable, the closer of the two is chosen.
     """
     def setup(self):
         super().setup()
-        self.active_path = None
-        self.active_path_index = 0
 
-    def choose_next_loc(self):
+    def choose_next_path(self):
         best_node = None
         best_node_score = 0
-        best_node_dist = 0
+        best_node_path = None
         for node in self.world.graph.nodes:
             if not self.visited(node):
-                score = 0
+                prob_nothing_here = 1;
                 pot_objs = self.arrangement_space.pot_objs_at(node)
                 path = self.world.graph.shortest_path(self.loc, node)
-                # Node is scored based on aggregate probability of novel finds
+                # Node is scored based on the probability of finding any object
+                # This is one minus the product of all the probabilities of the
+                # objects not appearing at this location
                 for pot_obj in pot_objs:
-                    score += self.arrangement_space.prob_obj(pot_obj, node)
+                    prob_nothing_here *= 1 - self.arrangement_space.prob_obj(pot_obj, node)
+                # score is the probability that something is here
+                score = 1 - prob_nothing_here
                 if best_node is None or \
                    score > best_node_score or \
-                   (score == best_node_score and path.cost < best_node_dist):
+                   (score == best_node_score and path.cost < best_node_path.cost):
                     best_node = node
                     best_node_score = score
-                    best_node_dist = path.cost
+                    best_node_path = path
         assert best_node is not None
-        return best_node
+        return best_node_path
 
     def run(self):
         # Collect objects at current location and update the occurrence space
@@ -40,14 +42,9 @@ class ProbAgent(agent.Agent):
         if self.done():
             return
 
-        # If no currently active path, pick a destination and generate one
-        if self.active_path is None or \
-           self.active_path_index == len(self.active_path):
-            dest = self.choose_next_loc()
-            self.active_path = \
-                self.world.graph.shortest_path(self.loc, dest).nodes
-            self.active_path_index = 1
+        # if self.active_path is None or \
+        #    self.active_path_index == len(self.active_path):
+        new_path = self.choose_next_path()
 
         # Visit next location in the active path
-        self.go(self.active_path[self.active_path_index])
-        self.active_path_index += 1
+        self.go(new_path.nodes[1])
